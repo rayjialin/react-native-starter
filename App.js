11127eead0d70f8e3df2c25f1d9d00e9935c9b77
rayjialin <ruijialin@gmail.com>
2018-06-28 23:29:03 -0400

    initial commit for react native starter project

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return React.createElement(Text, {style: styles.description}, "Search for dogs to adpot");
+  }
+}
+
+const styles = StyleSheet.create({
+	description: {
+		fontSize: 18,
+		textAlign: 'center',
+		color: '#656565',
+		marginTop: 65,
+	},
+});
